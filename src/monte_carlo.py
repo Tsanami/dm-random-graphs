78b1c74fb70ba222115261e5599353887811fcc6
ravil75 <112487461+ravil75@users.noreply.github.com>
2025-05-16 21:19:06 +0500

    Update monte_carlo.py

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -2,36 +2,54 @@ import numpy as np
 from .build_graph import build_knn_graph, build_distance_graph
 from .graph_analyzer import GraphAnalyzer
 
+
 def monte_carlo_simulation(
-    distribution,  # Функция генерации данных (generate_h0/generate_h1)
-    params: dict,   # Параметры распределения ({"mu":0, "sigma":1} и тп)
+    distribution,  # Функция генерации данных (generate_chi2/generate_chi)
+    params: dict,  # Параметры распределения
     n_samples: int = 1000,
     graph_type: str = "knn",
     graph_param: float | int = 3,  # k для KNN, d для дистанционного
-    metric: str = "count_triangles"  # Название характеристики из GraphAnalyzer
+    metric: str = "count_triangles",  # Название характеристики из GraphAnalyzer
+    metric_args: dict = None,  # Новый параметр для аргументов
 ) -> np.ndarray:
     """
     Выполняет симуляцию методом Монте-Карло для оценки распределения характеристики графа.
-    
+
     Возвращает:
         Массив значений характеристики для всех симуляций.
     """
+    if graph_type not in ["knn", "distance"]:
+        raise ValueError(
+            "Недопустимый тип графа. Используйте 'knn' или 'distance'."
+        )
+
     T_values = []
-    
+
     for _ in range(n_samples):
         # 1. Генерация данных
         data = distribution(**params)
-        
+
         # 2. Построение графа
         if graph_type == "knn":
-            adjacency = build_knn_graph(data, k=graph_param)
+            G = build_knn_graph(data, k=graph_param)
         else:
-            adjacency = build_distance_graph(data, d=graph_param)
-        
+            G = build_distance_graph(data, d=graph_param)
+
         # 3. Анализ характеристик
-        analyzer = GraphAnalyzer(adjacency)
-        T = getattr(analyzer, metric)()  # Динамический выбор характеристики
-        
+        analyzer = GraphAnalyzer(G)
+
+        if not hasattr(analyzer, metric):
+            raise ValueError(
+                f"Метрика {metric} не существует в GraphAnalyzer."
+            )
+
+        if metric_args is None:
+            metric_args = {}
+
+        T = getattr(analyzer, metric)(
+            **metric_args
+        )  # Динамический выбор характеристики
+
         T_values.append(T)
-    
+
     return np.array(T_values)
