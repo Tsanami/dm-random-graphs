3401cb948672a14753929bb77be0de4a0305ed6a
ravil75 <112487461+ravil75@users.noreply.github.com>
2025-05-16 21:44:08 +0500

    Update distribution_generators.py

--- src/distribution_generators.py
+++ src/distribution_generators.py
@@ -1,9 +1,11 @@
 from scipy.stats import chi, chi2
 import numpy as np
 
+
 def generate_chi2(nu: int, n: int) -> np.ndarray:
     """Генерация данных из χ²"""
     return chi2.rvs(df=nu, size=n)
+
 
 def generate_chi(nu: int, n: int) -> np.ndarray:
     """Генерация данных из χ"""
