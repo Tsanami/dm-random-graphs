a8f64bdc161f9dab378ad9515499e6184cfa5702
ravil75 <112487461+ravil75@users.noreply.github.com>
2025-05-28 18:48:14 +0500

    Update test_graph_analyzer.py

--- tests/test_graph_analyzer.py
+++ tests/test_graph_analyzer.py
@@ -1,9 +1,10 @@
 import numpy as np
 import networkx as nx
 from src.graph_analyzer import GraphAnalyzer
+from src.build_graph import build_distance_graph
 
 
-def test_max_degree_and_chromatic_and_clique_and_triangles():
+def test_max_degree_and_chromatic_and_triangles():
     # Создаём граф: линия из трёх узлов
     G = nx.path_graph(3)
     ga = GraphAnalyzer(G)
@@ -13,11 +14,9 @@ def test_max_degree_and_chromatic_and_clique_and_triangles():
     assert ga.chromatic_number() == 2
     # Число треугольников в пути = 0
     assert ga.count_triangles() == 0
-    # Кликовое число (размер наибольшей клики) для пути = 2
-    assert ga.clique_number() == 2
 
 
-def test_graph_analyzer_array_input_with_clique_and_triangles():
+def test_graph_analyzer_array_input_with_triangles():
     # Матрица смежности для пути из трёх узлов
     A = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
     ga = GraphAnalyzer(A)
@@ -27,11 +26,9 @@ def test_graph_analyzer_array_input_with_clique_and_triangles():
     assert ga.chromatic_number() == 2
     # Число треугольников = 0
     assert ga.count_triangles() == 0
-    # Кликовое число = 2
-    assert ga.clique_number() == 2
 
 
-def test_complete_graph_triangles_and_clique():
+def test_complete_graph_triangles():
     # Полный граф на трех узлах (клика K3)
     Gk3 = nx.complete_graph(3)
     ga_k3 = GraphAnalyzer(Gk3)
@@ -41,5 +38,27 @@ def test_complete_graph_triangles_and_clique():
     assert ga_k3.chromatic_number() == 3
     # В K3 ровно один треугольник
     assert ga_k3.count_triangles() == 1
-    # Кликовое число для K3 = 3
-    assert ga_k3.clique_number() == 3
+
+
+def test_clique_number_with_threshold():
+    # Узлы на координатах 0, 1, 3, d=1
+    G = nx.Graph()
+    coords = [0.0, 1.0, 3.0]
+    for i, x in enumerate(coords):
+        G.add_node(i, x=x)
+    ga = GraphAnalyzer(G)
+    # d=1: интервал ширины 1 может поймать точки [0,1] -> клика размер 2
+    assert ga.clique_number(d=1.0) == 2
+    # d=3: интервал ширины 3 покрывает все точки -> клика размер 3
+    assert ga.clique_number(d=3.0) == 3
+
+
+def test_clique_number_on_distance_graph():
+    # Строим дистанционный граф из координат через build_distance_graph
+    data = np.array([0.0, 1.0, 2.0, 5.0])
+    d = 2.0
+    G = build_distance_graph(data, d=d)
+    ga = GraphAnalyzer(G)
+    # Наибольшая клика должна соответствовать максимальному количеству точек
+    # попадающих в интервал длины d: [0,2] -> 3 точки (0.0,1.0,2.0)
+    assert ga.clique_number(d=d) == 3
